e24761cd31f3f749058a2e8951f0ebe42149a95e
Trevor Hughes-Smith <trevorhughes8@yahoo.com>
2021-03-04 12:30:33 -0500

    refactoring

--- tenmo-server/src/main/java/com/techelevator/tenmo/model/Transfers.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/model/Transfers.java
@@ -5,31 +5,32 @@ import java.math.BigDecimal;
 public class Transfers {
 	
 	private int transferID;
-	private int transferTypeID;
-	private int transferStatusID;
+	private String transferTypeDesription;
+	private String transferStatusDescription;
 	private int accountFrom;
 	private int accountTo;
 	private BigDecimal amount;
 	
 	
+	public String getTransferTypeDesription() {
+		return transferTypeDesription;
+	}
+	public void setTransferTypeDesription(String transferTypeDesription) {
+		this.transferTypeDesription = transferTypeDesription;
+	}
+	public String getTransferStatusDescription() {
+		return transferStatusDescription;
+	}
+	public void setTransferStatusDescription(String transferStatusDescription) {
+		this.transferStatusDescription = transferStatusDescription;
+	}
 	public int getTransferID() {
 		return transferID;
 	}
 	public void setTransferID(int transferID) {
 		this.transferID = transferID;
 	}
-	public int getTransferTypeID() {
-		return transferTypeID;
-	}
-	public void setTransferTypeID(int transferTypeID) {
-		this.transferTypeID = transferTypeID;
-	}
-	public int getTransferStatusID() {
-		return transferStatusID;
-	}
-	public void setTransferStatusID(int transferStatusID) {
-		this.transferStatusID = transferStatusID;
-	}
+
 	public int getAccountFrom() {
 		return accountFrom;
 	}
